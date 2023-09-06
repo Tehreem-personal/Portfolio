5d447b4bd5fd24fc02b325410f928070de0d81fd
Tehreem-personal <tehreemawan3@gmail.com>
2023-09-06 10:02:06 -0700

    project added

--- src/Components/Work.js
+++ src/Components/Work.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import WorkImg from "../assets/Berain.JPG"
 import contact from "../assets/Contact.JPG"
+import food from "../assets/FoodApp.JPG"
 import quote from "../assets/QuoteGenerator.JPG"
 import sushi from "../assets/Sushi.JPG"
 import aljazeirah from "../assets/ALjazeirah.JPG"
@@ -23,6 +24,18 @@ const Work = () => {
                         </span>
                         <div>
                             <a href="https://sushi-app-three.vercel.app/"><button className='text-center rounded-lg px-4 py-3 m-3 bg-white text-gray-700 font-bold text-lg'> Demo</button></a>
+                            <a href="/"><button  className='text-center rounded-lg px-4 py-3 m-3 bg-white text-gray-700 font-bold text-lg'>Code</button></a>
+                        </div>
+                    </div>
+                </div>
+                <div style={{backgroundImage:`url(${food})`}}
+                className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                    <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-2xl font-bold text-white tracking-wider'>
+                        ReactJS-Tailwind Food App
+                        </span>
+                        <div>
+                            <a href="https://reactjs-tailwind-food-app.vercel.app/"><button className='text-center rounded-lg px-4 py-3 m-3 bg-white text-gray-700 font-bold text-lg'> Demo</button></a>
                             <a href="/"><button  className='text-center rounded-lg px-4 py-3 m-3 bg-white text-gray-700 font-bold text-lg'>Code</button></a>
                         </div>
                     </div>
